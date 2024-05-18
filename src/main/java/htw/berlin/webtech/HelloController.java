8a20983ad4550dad4c227ae043f87ee9e88a6961
HowdoIC0DE <137148316+HowdoIC0DE@users.noreply.github.com>
2024-05-18 17:17:12 +0200

    Changed to HTML-file
    Replaced the basic String return with a ModelAndView.

--- src/main/java/htw/berlin/webtech/HelloController.java
+++ src/main/java/htw/berlin/webtech/HelloController.java
@@ -1,14 +1,18 @@
 package htw.berlin.webtech;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("index.html");
+        return view;
     }
 
     @GetMapping("/test")
